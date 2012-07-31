624f91ba4d8300ca019b5fa54c8e4755c6a754cf
Makers_F <makers.f@gmail.com>
2012-07-31 22:25:37 +0200

    Now perform the pixel perfect collision check against every RectangularShape. The rectangular shape is considered all solid

--- src/com/makersf/andengine/extension/collisions/entity/sprite/PixelPerfectAnimatedSprite.java
+++ src/com/makersf/andengine/extension/collisions/entity/sprite/PixelPerfectAnimatedSprite.java
@@ -10,9 +10,12 @@ import com.makersf.andengine.extension.collisions.entity.shape.IPixelPerfectShap
 import com.makersf.andengine.extension.collisions.opengl.texture.region.PixelPerfectTiledTextureRegion;
 import com.makersf.andengine.extension.collisions.pixelperfect.PixelPerfectCollisionChecker;
 import com.makersf.andengine.extension.collisions.pixelperfect.IPixelPerfectMask;
+import com.makersf.andengine.extension.collisions.pixelperfect.RectangularPixelPerfectMask;
 
 public class PixelPerfectAnimatedSprite extends AnimatedSprite implements IPixelPerfectShape{
 
+	private RectangularPixelPerfectMask mReusableRectangularPixelPerfectMask = new RectangularPixelPerfectMask(0, 0);
+	
 	public PixelPerfectAnimatedSprite(float pX, float pY, float pWidth,
 			float pHeight, PixelPerfectTiledTextureRegion pTiledTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager) {
@@ -33,8 +36,22 @@ public class PixelPerfectAnimatedSprite extends AnimatedSprite implements IPixel
 		return ((PixelPerfectTiledTextureRegion)mTextureRegion).getPixelMask(getCurrentTileIndex());
 	}
 
-	public <T extends RectangularShape&IPixelPerfectShape>boolean collidesWith(final T pOtherPPShape) {
-		return super.collidesWith(pOtherPPShape) && 
-				PixelPerfectCollisionChecker.checkCollision(this, this.getPixelPerfectMask(), pOtherPPShape, pOtherPPShape.getPixelPerfectMask());
+	public boolean collidesWith(final RectangularShape pOtherShape) {
+		if(super.collidesWith(pOtherShape))
+		{
+			if(pOtherShape instanceof IPixelPerfectShape)
+				return PixelPerfectCollisionChecker.checkCollision(this, this.getPixelPerfectMask(), pOtherShape, ((IPixelPerfectShape)pOtherShape).getPixelPerfectMask());
+			else
+			{
+				//syncronized since if you check 2 different RectShapes in 2 different thread, the mReusableRecShape can be changed while it is still used by the other thread
+				//but i think it is really unlikely that parellal check can occour, then this syncronization is almost for free
+				synchronized (mReusableRectangularPixelPerfectMask) {
+					mReusableRectangularPixelPerfectMask.setTo((int) pOtherShape.getWidth(), (int) pOtherShape.getHeight());
+					return PixelPerfectCollisionChecker.checkCollision(this, this.getPixelPerfectMask(), pOtherShape, mReusableRectangularPixelPerfectMask);
+				}
+			}
+		}
+		else
+			return false;
 	}
 }
--- src/com/makersf/andengine/extension/collisions/entity/sprite/PixelPerfectSprite.java
+++ src/com/makersf/andengine/extension/collisions/entity/sprite/PixelPerfectSprite.java
@@ -10,9 +10,12 @@ import com.makersf.andengine.extension.collisions.entity.shape.IPixelPerfectShap
 import com.makersf.andengine.extension.collisions.opengl.texture.region.PixelPerfectTextureRegion;
 import com.makersf.andengine.extension.collisions.pixelperfect.PixelPerfectCollisionChecker;
 import com.makersf.andengine.extension.collisions.pixelperfect.IPixelPerfectMask;
+import com.makersf.andengine.extension.collisions.pixelperfect.RectangularPixelPerfectMask;
 
 public class PixelPerfectSprite extends Sprite implements IPixelPerfectShape{
 
+	private RectangularPixelPerfectMask mReusableRectangularPixelPerfectMask = new RectangularPixelPerfectMask(0, 0);
+	
 	public PixelPerfectSprite(float pX, float pY, float pWidth, float pHeight,
 			PixelPerfectTextureRegion pTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager) {
@@ -32,9 +35,23 @@ public class PixelPerfectSprite extends Sprite implements IPixelPerfectShape{
 		return ((PixelPerfectTextureRegion)mTextureRegion).getPixelMask();
 	}
 	
-	public <T extends RectangularShape&IPixelPerfectShape>boolean collidesWith(final T pOtherPPShape) {
-		return super.collidesWith(pOtherPPShape) && 
-				PixelPerfectCollisionChecker.checkCollision(this, this.getPixelPerfectMask(), pOtherPPShape, pOtherPPShape.getPixelPerfectMask());
+	public boolean collidesWith(final RectangularShape pOtherShape) {
+		if(super.collidesWith(pOtherShape))
+		{
+			if(pOtherShape instanceof IPixelPerfectShape)
+				return PixelPerfectCollisionChecker.checkCollision(this, this.getPixelPerfectMask(), pOtherShape, ((IPixelPerfectShape)pOtherShape).getPixelPerfectMask());
+			else
+			{
+				//syncronized since if you check 2 different RectShapes in 2 different thread, the mReusableRecShape can be changed while it is still used by the other thread
+				//but i think it is really unlikely that parellal check can occour, then this syncronization is almost for free
+				synchronized (mReusableRectangularPixelPerfectMask) {
+					mReusableRectangularPixelPerfectMask.setTo((int) pOtherShape.getWidth(), (int) pOtherShape.getHeight());
+					return PixelPerfectCollisionChecker.checkCollision(this, this.getPixelPerfectMask(), pOtherShape, mReusableRectangularPixelPerfectMask);
+				}
+			}
+		}
+		else
+			return false;
 	}
 
 }
