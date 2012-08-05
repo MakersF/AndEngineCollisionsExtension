431404db6775348f0b9edfb0b84de9354fb2afe3
Makers_F <makers.f@gmail.com>
2012-08-05 18:26:18 +0200

    Added a logger to control the performances of the collision check

---
+++ src/com/makersf/andengine/extension/collisions/CollisionLogger.java
@@ -0,0 +1,64 @@
+package com.makersf.andengine.extension.collisions;
+
+import android.os.SystemClock;
+import android.util.Log;
+
+public class CollisionLogger {
+
+	private long mCollisionStartTime;
+
+	private long mTotalSuccesfullCollisionTime;
+	private int mSuccesfullCollisions;
+	private long mMinSuccesfullTime = Long.MAX_VALUE;
+	private long mMaxSuccesfullTime;
+	
+	private long mTotalUnsuccesfullCollisionTime;
+	private int mUnsuccesfullCollisions;
+	private long mMinUnsuccesfullTime = Long.MAX_VALUE;
+	private long mMaxUnsuccesfullTime;
+
+	
+	private long getCurrentTime() {
+		return SystemClock.elapsedRealtime();
+	}
+	
+	public void startCollisionCheck() {
+		mCollisionStartTime = getCurrentTime();
+	}
+	
+	public void endCollisionCheck(boolean pCollisionSuccesfull) {
+		final long currentTime = getCurrentTime();
+		final long elapsedTime = currentTime - mCollisionStartTime;
+		if(pCollisionSuccesfull) {
+			mTotalSuccesfullCollisionTime += elapsedTime;
+			mSuccesfullCollisions++;
+			
+			mMinSuccesfullTime = Math.min(mMinSuccesfullTime, elapsedTime);
+			mMaxSuccesfullTime = Math.max(mMaxSuccesfullTime, elapsedTime);
+		}
+		else {
+			mTotalUnsuccesfullCollisionTime += elapsedTime;
+			mUnsuccesfullCollisions++;
+			
+			mMinUnsuccesfullTime = Math.min(mMinUnsuccesfullTime, elapsedTime);
+			mMaxUnsuccesfullTime = Math.max(mMaxUnsuccesfullTime, elapsedTime);
+		}
+	}
+	
+	public void printStatistics(boolean pResetStatistics) {
+		Log.i("COLLISION_LOGGER", (mSuccesfullCollisions + mUnsuccesfullCollisions) + " in " + (mTotalSuccesfullCollisionTime + mTotalUnsuccesfullCollisionTime) + " ms, Avarage: " + (((float) mTotalSuccesfullCollisionTime + (float) mTotalUnsuccesfullCollisionTime)/((float) mSuccesfullCollisions + (float) mUnsuccesfullCollisions)) + " ms. \n" +
+				mSuccesfullCollisions + " succesfull collisions in " + mTotalSuccesfullCollisionTime + " ms. Min: " + mMinSuccesfullTime + ", Max: " + mMaxSuccesfullTime + ", Avarage: " + ((float) mTotalSuccesfullCollisionTime)/((float) mSuccesfullCollisions) + ".\n" +
+				mUnsuccesfullCollisions + " unsuccesfull collisions in " + mTotalUnsuccesfullCollisionTime + " ms. Min: " + mMinUnsuccesfullTime + ", Max: " + mMaxUnsuccesfullTime + ", Avarage: " + ((float) mTotalUnsuccesfullCollisionTime)/((float) mUnsuccesfullCollisions) + ".");
+		if(pResetStatistics) {			
+			mTotalSuccesfullCollisionTime = 0;
+			mSuccesfullCollisions = 0;
+			mMinSuccesfullTime = Long.MAX_VALUE;
+			mMaxSuccesfullTime = 0;
+			
+			mTotalUnsuccesfullCollisionTime = 0;
+			mUnsuccesfullCollisions = 0;
+			mMinUnsuccesfullTime = Long.MAX_VALUE;
+			mMaxUnsuccesfullTime = 0;
+		}
+	}
+}
