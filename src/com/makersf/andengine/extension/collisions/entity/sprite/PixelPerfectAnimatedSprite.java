9ccca13314cbd80c7b6724fdb8f93c8745dac1f7
Makers_F <makers.f@gmail.com>
2013-03-28 16:14:35 +0100

    Allow to instantiate PixelPerfectSprite and PixelPerfectAnimatedSprite with custom IPixelPerfectMask, without the need of IPixelPerfectTextureRegion

--- src/com/makersf/andengine/extension/collisions/entity/sprite/PixelPerfectAnimatedSprite.java
+++ src/com/makersf/andengine/extension/collisions/entity/sprite/PixelPerfectAnimatedSprite.java
@@ -3,6 +3,7 @@ package com.makersf.andengine.extension.collisions.entity.sprite;
 import org.andengine.entity.shape.RectangularShape;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.opengl.shader.ShaderProgram;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
 import org.andengine.opengl.vbo.DrawType;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
@@ -23,17 +24,39 @@ public class PixelPerfectAnimatedSprite extends AnimatedSprite implements IPixel
 	
 	private static boolean USE_PIXELPERFECT_COLLISION_FOR_EVERY_RECTANGULAR_SHAPE = true;
 
+	private IPixelPerfectMask[] mPixelMasks;
+
 	public PixelPerfectAnimatedSprite(float pX, float pY, PixelPerfectTiledTextureRegion pTiledTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager) {
 		super(pX, pY, pTiledTextureRegion,
 				pVertexBufferObjectManager);
+		final int tileCount = pTiledTextureRegion.getTileCount();
+		mPixelMasks = new IPixelPerfectMask[tileCount];
+		for(int i = 0; i < tileCount; i++) {
+			mPixelMasks[i] = pTiledTextureRegion.getPixelMask(i);
+		}
 	}
-	
+
+	public PixelPerfectAnimatedSprite(float pX, float pY, ITiledTextureRegion pTiledTextureRegion, IPixelPerfectMask[] pPixelMasks,
+			VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pTiledTextureRegion,
+				pVertexBufferObjectManager);
+		if(pTiledTextureRegion.getTileCount() != pPixelMasks.length)
+			throw new IllegalArgumentException("The number of PixelMasks must be equal to the number of tiles!");
+
+		mPixelMasks = pPixelMasks;
+	}
+
 	public PixelPerfectAnimatedSprite(float pX, float pY, float pWidth,
 			float pHeight, PixelPerfectTiledTextureRegion pTiledTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager) {
 		super(pX, pY, pWidth, pHeight, pTiledTextureRegion,
 				pVertexBufferObjectManager);
+		final int tileCount = pTiledTextureRegion.getTileCount();
+		mPixelMasks = new IPixelPerfectMask[tileCount];
+		for(int i = 0; i < tileCount; i++) {
+			mPixelMasks[i] = pTiledTextureRegion.getPixelMask(i);
+		}
 	}
 	
 	public PixelPerfectAnimatedSprite(float pX, float pY, float pWidth,
@@ -42,15 +65,20 @@ public class PixelPerfectAnimatedSprite extends AnimatedSprite implements IPixel
 			DrawType pDrawType, ShaderProgram pShaderProgram) {
 		super(pX, pY, pWidth, pHeight, pTiledTextureRegion, pVertexBufferObjectManager,
 				pDrawType, pShaderProgram);
+		final int tileCount = pTiledTextureRegion.getTileCount();
+		mPixelMasks = new IPixelPerfectMask[tileCount];
+		for(int i = 0; i < tileCount; i++) {
+			mPixelMasks[i] = pTiledTextureRegion.getPixelMask(i);
+		}
 	}
 
 	@Override
 	public IPixelPerfectMask getPixelPerfectMask() {
-		return ((PixelPerfectTiledTextureRegion)mTextureRegion).getPixelMask(getCurrentTileIndex());
+		return mPixelMasks[getCurrentTileIndex()];
 	}
 
 	public IPixelPerfectMask getPixelMask(final int pTileIndex) {
-		return ((PixelPerfectTiledTextureRegion)mTextureRegion).getPixelMask(pTileIndex);
+		return mPixelMasks[pTileIndex];
 	}
 
 	public boolean collidesWith(final RectangularShape pOtherShape) {
--- src/com/makersf/andengine/extension/collisions/entity/sprite/PixelPerfectSprite.java
+++ src/com/makersf/andengine/extension/collisions/entity/sprite/PixelPerfectSprite.java
@@ -3,6 +3,7 @@ package com.makersf.andengine.extension.collisions.entity.sprite;
 import org.andengine.entity.shape.RectangularShape;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.opengl.shader.ShaderProgram;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.DrawType;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
@@ -23,16 +24,28 @@ public class PixelPerfectSprite extends Sprite implements IPixelPerfectShape{
 	
 	private static boolean USE_PIXELPERFECT_COLLISION_FOR_EVERY_RECTANGULAR_SHAPE = true;
 
+	private IPixelPerfectMask mPixelMask;
+
 	public PixelPerfectSprite(float pX, float pY,
 			PixelPerfectTextureRegion pTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager) {
 		super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
+		mPixelMask = pTextureRegion.getPixelMask();
 	}
-	
+
+	public PixelPerfectSprite(float pX, float pY,
+			ITextureRegion pTextureRegion,
+			IPixelPerfectMask pPixelMask,
+			VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
+		mPixelMask = pPixelMask;
+	}
+
 	public PixelPerfectSprite(float pX, float pY, float pWidth, float pHeight,
 			PixelPerfectTextureRegion pTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager) {
 		super(pX, pY, pWidth, pHeight, pTextureRegion, pVertexBufferObjectManager);
+		mPixelMask = pTextureRegion.getPixelMask();
 	}
 
 	public PixelPerfectSprite(float pX, float pY, float pWidth, float pHeight,
@@ -41,11 +54,12 @@ public class PixelPerfectSprite extends Sprite implements IPixelPerfectShape{
 			DrawType pDrawType, ShaderProgram pShaderProgram) {
 		super(pX, pY, pWidth, pHeight, pTextureRegion, pVertexBufferObjectManager,
 				pDrawType, pShaderProgram);
+		mPixelMask = pTextureRegion.getPixelMask();
 	}
 
 	@Override
 	public IPixelPerfectMask getPixelPerfectMask() {
-		return ((PixelPerfectTextureRegion)mTextureRegion).getPixelMask();
+		return mPixelMask;
 	}
 	
 	public boolean collidesWith(final RectangularShape pOtherShape) {
