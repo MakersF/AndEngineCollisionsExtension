26c310114b1889d3374523ebf512b0a6c2fceb6d
Makers_F <makers.f.dev@gmail.com>
2013-10-04 13:20:20 +0200

    Moved the core part of the checking to another project

--- src/com/makersf/andengine/extension/collisions/pixelperfect/masks/IPixelPerfectMaskFactory.java
+++
@@ -1,8 +0,0 @@
-package com.makersf.andengine.extension.collisions.pixelperfect.masks;
-
-import java.nio.ByteBuffer;
-
-public interface IPixelPerfectMaskFactory {
-
-	public IPixelPerfectMask getIPixelPerfectMask(int pWidth, int pHeight, ByteBuffer pByteBuffer);
-}
